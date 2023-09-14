e0130ea3da4f245d4c6136fdc60a39665d593511
Kdddru <125441996+Kdddru@users.noreply.github.com>
2023-09-14 20:22:20 +0900

    0913

---
+++ 0913.js
@@ -0,0 +1,28 @@
+var 동물 = '기린';
+var 동물2 = { name: "기린", age: 25 };
+//const => 재할당 방지 but 프로퍼티 수정은 가능
+var 나 = {
+    name: 'kimdu'
+};
+var postion = {
+    x: 10,
+    y: 20,
+};
+var obj1 = {
+    color: 'white',
+    size: 20,
+    position: [1, 2, 3, 4]
+};
+var me = {
+    name: 'kim',
+    phone: 123,
+    email: 'abc@naver.com'
+};
+var newPerson = {
+    name: 'kim',
+    phone: 123,
+    email: 'abc@naver.com',
+    adult: true
+};
+//리터럴
+var kim;
---
+++ 0913.ts
@@ -0,0 +1,76 @@
+// type alias
+// type 재정의 불가능
+type Animal = string| undefined;
+
+let 동물 :Animal = '기린';
+
+
+type Aniaml2 = {name :string, age : number};
+let 동물2 :Aniaml2 = {name : "기린", age : 25};
+
+type information = {
+  readonly name? : string  
+  //수정할수없음  
+  //? ==> 타입 | undefined
+};
+
+//const => 재할당 방지 but 프로퍼티 수정은 가능
+
+const 나:information= {
+  name : 'kimdu'
+}
+
+//error
+//나.name = '김두현';
+
+//타입 합치기 (extend)
+
+type PositionX = { x:number };
+type PositionY = { y :number };
+
+type NewType = PositionX&PositionY;
+
+let postion : NewType ={
+  x : 10,
+  y : 20,
+};
+
+
+type obj = { 
+  color : string,
+  size : number
+  readonly position : number[]
+}
+
+let obj1 : obj ={
+  color : 'white',
+  size : 20,
+  position : [1,2,3,4]
+}
+
+type personalInformation ={
+  name : string,
+  phone? : number,
+  email : string
+}
+
+let me :personalInformation = {
+  name : 'kim',
+  phone : 123,
+  email : 'abc@naver.com'
+}
+
+type Adult = { adult :boolean }
+
+type newPersonalInformation = personalInformation & Adult
+
+let newPerson: newPersonalInformation ={
+  name : 'kim',
+  phone : 123,
+  email : 'abc@naver.com',
+  adult : true
+}
+
+//리터럴
+let kim :'대머리';
+
